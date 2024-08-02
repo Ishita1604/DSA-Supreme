8c27d5e9a2111b94ec4a76d091b07759eb997a7d
Ishita Gupta <112006649+Ishita1604@users.noreply.github.com>
2024-08-02 23:56:28 +0530

    Find the Even Numbers in a Given Range

---
+++ 1. Basic Programming/5_EvenInRange.cpp
@@ -0,0 +1,20 @@
+// Find even numbers in [a, b] range.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter the Value of a: ";
+    cin >> a;
+    cout << "Enter the Value of b: ";
+    cin >> b;
+
+    if (a % 2 != 0)
+        a = a + 1;
+
+    for (int i = a; i <= b; i = i + 2)
+        cout << i << "  " << endl;
+
+    return 0;
+}
