6517bf3cf9e07523957ce8dd2b6cbe8c49795831
Ishita Gupta <112006649+Ishita1604@users.noreply.github.com>
2024-09-22 23:39:49 +0530

    For Loop

---
+++ 1. Basic Programming/10_ForLoop.cpp
@@ -0,0 +1,19 @@
+// For Loop:
+// To repeat code "n" number of times.
+// Instead of writing same code "n" times.
+// Syntax: for (initialisation; condition; update)
+// We use the "counter" variable to maintain number of times to run the code.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "C++ is awesome!" << endl;
+    }
+
+    return 0;
+}
