99dd722a8af7b4ef31313829c9c4298f389ca948
Ishita Gupta <112006649+Ishita1604@users.noreply.github.com>
2024-10-22 19:35:51 +0530

    Reverse Digits of Number

---
+++ 1. Basic Programming/20_Reverse.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// Reverse the digits of the number.
+
+int main()
+{
+    int n;
+    cout << "Enter a Number: ";
+    cin >> n;
+    int reverse = 0;
+
+    while (n > 0)
+    {
+        int last_digit = n % 10;
+        reverse = reverse * 10 + last_digit;
+        n = n / 10;
+    }
+    cout << reverse << endl;
+
+    return 0;
+}
